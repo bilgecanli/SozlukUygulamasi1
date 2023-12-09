fddcb34592ce1612a2bd7fa630e84397879e8e18
mehmet <mehmetsaidcanli@gmail.com>
2023-12-09 17:18:37 +0300

    Initial Commit

---
+++ SozlukUygulamasi1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SozlukUygulamasi1
+//
+//  Created by Mehmet Said Canlı on 9.12.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
